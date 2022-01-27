5973efa331706ae1d6cb777f793b4b44fd0f217b
Aleksey Vasilenko <vasilenko.aleksey.v@gmail.com>
2022-01-27 15:57:24 +0200

    commit 27/01 - 1

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,46 +1,27 @@
 package org.example;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.StringWriter;
 import java.util.Date;
 
 @JsonAutoDetect
 public class Client {
-    private ClientPhoneNumber clientPhoneNumber;
-    private ClientPersonalInfo clientPersonalInfo;
-    private int client_id;
-    private long createdClientDate;
-    private long deletedClientDate;
+    private String fullName;
+    private String phoneType;
 
     public Client(){}
 
-    public ClientPhoneNumber getClientPhoneNumber() {
-        return clientPhoneNumber;
+    public String getFullName() {
+        return fullName;
     }
-    public void setClientPhoneNumber(ClientPhoneNumber clientPhoneNumber) {
-        this.clientPhoneNumber = clientPhoneNumber;
+    public String getPhoneType() {
+        return phoneType;
     }
-    public ClientPersonalInfo getClientPersonalInfo() {
-        return clientPersonalInfo;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
-    public void setClientPersonalInfo(ClientPersonalInfo clientPersonalInfo) {
-        this.clientPersonalInfo = clientPersonalInfo;
-    }
-    public int getClient_id() {
-        return client_id;
-    }
-    public void setClient_id(int client_id) {
-        this.client_id = client_id;
-    }
-    public long getCreatedClientDate() {
-        return createdClientDate;
-    }
-    public void setCreatedClientDate(long createdClientDate) {
-        this.createdClientDate = createdClientDate;
-    }
-    public long getDeletedClientDate() {
-        return deletedClientDate;
-    }
-    public void setDeletedClientDate(long deletedClientDate) {
-        this.deletedClientDate = deletedClientDate;
+    public void setPhoneType(String phoneType) {
+        this.phoneType = phoneType;
     }
 }
--- src/main/java/org/example/RestService.java
+++ src/main/java/org/example/RestService.java
@@ -1,6 +1,10 @@
 package org.example;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -8,49 +12,46 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Path("/")
 public class RestService {
     private static final AtomicInteger currentId = new AtomicInteger();
-    private static final Map<String, String> links = new ConcurrentHashMap<>();
-
-    public static String getLinks(String id) {
-        return links.get(id);
-    }
+    private static final Map<Integer, Client> links = new ConcurrentHashMap<>();
 
     @POST // add new
-    public Response postMessage(final String url) {
+    public Response postMessage(final String requestBody) throws IOException {
         final int id = currentId.getAndIncrement();
-        links.put(String.valueOf(id), url.toString());
+        StringReader stringReader = new StringReader(requestBody);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Client client = objectMapper.readValue(stringReader, Client.class);
+        links.put(id, client);
         return Response.ok(id).build();
     }
 
     @GET // get info
     @Path("/{id}")
-    public Response getMessage(@PathParam("id") String id) {
-        if (id == null || id.isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
-        final String url = links.get(id);
-        if (url == null) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
-        return Response.ok(url).build();
+    public Response getMessage(@PathParam("id") int id) throws IOException {
+        Client client = links.get(id);
+        StringWriter stringWriter = new StringWriter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(stringWriter, client);
+        return Response.ok(stringWriter.toString()).build();
     }
 
     @PUT // edit
     @Path("/{id}")
-    public Response putMessage(@PathParam("id") String id, final String url) {
-        if (id == null || id.isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
+    public Response putMessage(@PathParam("id") int id, final String requestBody) throws IOException {
+        Client client = links.get(id);
+        StringReader stringReader = new StringReader(requestBody);
+        ObjectMapper objectMapperInput = new ObjectMapper();
+        client = objectMapperInput.readValue(stringReader, Client.class);
         links.remove(id);
-        links.put(String.valueOf(id), url.toString());
-        return Response.ok(id).build();
+        links.put(id, client);
+        StringWriter stringWriter = new StringWriter();
+        ObjectMapper objectMapperOutput = new ObjectMapper();
+        objectMapperOutput.writeValue(stringWriter, client);
+        return Response.ok(stringWriter.toString()).build();
     }
 
     @DELETE // delete
     @Path("/{id}")
-    public Response deleteMessage(@PathParam("id") String id) {
-        if (id == null || id.isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
+    public Response deleteMessage(@PathParam("id") int id) {
         links.remove(id);
         return Response.ok(id).build();
     }
